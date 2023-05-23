05a4b35b916b36bc64d40d8748ec739df50ed0a4
RayanCorporation <81195088+RayanCorporation@users.noreply.github.com>
2023-05-23 12:29:26 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,13 +9,6 @@ def load_quotes(filename):
         if line:
             quotes.append(line)
     return quotes
-
-def add_quote(quotes, filename):
-    new_quote = input("Enter a new quote: ")
-    quotes.append(new_quote)
-    
-    with open(filename, 'a') as file:
-        file.write(new_quote)
         
 def random_quote(quotes):
     random_quote = random.choice(quotes)
